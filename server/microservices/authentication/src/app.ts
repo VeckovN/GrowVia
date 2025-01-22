442d0139ee54fb0d5f4b6fd8d63149ba0c8ee5e7
VeckovN <novaknolewow@gmail.com>
2025-01-22 20:25:33 +0100

    Move code from init function

--- server/microservices/authentication/src/app.ts
+++ server/microservices/authentication/src/app.ts
@@ -1,16 +1,11 @@
 import { Logger } from "winston";
 import { winstonLogger } from "@veckovn/growvia-shared";
 import express, { Express } from "express";
-// import {start} from "@gateway/server";
+import {start} from "@authentication/server";
 import { config } from '@authentication/config';
 
 const log: Logger = winstonLogger(`${config.ELASTICSEARCH_URL}`, 'authenticationService', 'debug');
 
-function Init(): void{
-    const app:Express = express();
-    // start(app);
-    
-    log.info("Authentication service successfully initialized")
-}
-
-Init();
+const app:Express = express();
+start(app);
+log.info("Authentication service successfully initialized")
