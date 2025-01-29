5e9f0afe3efde22f5fbe3d6d8e915e01d8677302
VeckovN <novaknolewow@gmail.com>
2025-01-29 11:52:18 +0100

    Use defined interface from the shared library

--- server/microservices/gateway/src/authMiddleware.ts
+++ server/microservices/gateway/src/authMiddleware.ts
@@ -1,35 +1,19 @@
-//we have one token for Client->APiGateway 
-//and another for ApiGataway->Microservices
-
+//we have one token for Client->APiGateway  (JWT_TOKEN)
+//and another for ApiGataway->Microservices (GATEWAY_JWT_TOKEN)
 import { Request, Response, NextFunction } from "express";
+import { AuthPayloadInterface } from '@veckovn/growvia-shared';
 import { config } from '@gateway/config';
 import { verify } from "jsonwebtoken";
 
-//this chekcking token Between Client and ApiGateway
-//Every request that comming from client will be checked here does is token valid
-
-//interface (should be in shared library)
-interface AuthPayloadInterface {
-    id:number,
-    username: string,
-    email: string,
-    iat?: number,
-}
-
-declare global {
-    namespace Express {
-        interface Request {
-            currentUser?: AuthPayloadInterface;
-        }
-    }
-}
-
+//ON every request (that need user to be logged in) it checking does it have jwtToken in session
+//and is it valid -> then return req.currentUser as object with user data:AuthPayloadInterface
 export function verifyUser(req:Request, _res:Response, next:NextFunction):void {
     
     if(!req.session?.jwtToken) //if user doesn't have session (not logged)
         throw new Error("Token isnt' avaliable, Login again!"); //use Error hanlder function from shared libraryu
 
     try{
+        //Request must come from the APIGATEWAY(not directly from client allowed) -> verify it with GATEWAY_JWT_TOKEN
         const payload: AuthPayloadInterface = verify(req.session?.jwtToken, `${config.GATEWAY_JWT_TOKEN}`) as AuthPayloadInterface;
         req.currentUser = payload;  
     }
@@ -40,7 +24,7 @@ export function verifyUser(req:Request, _res:Response, next:NextFunction):void {
     next();
 }   
 
-
+//Authenitcated(Looged) user has currentUser
 export function checkUserAuth(req:Request, _res:Response, next:NextFunction):void {
     if(!req.currentUser){
         throw new Error("User isn't authenticated for this route");
@@ -48,4 +32,3 @@ export function checkUserAuth(req:Request, _res:Response, next:NextFunction):voi
 
     next();
 }
-
