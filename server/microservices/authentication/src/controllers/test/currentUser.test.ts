a4e1f6e81577c7524f362270e449eb542f10e186
VeckovN <novaknolewow@gmail.com>
2025-02-04 09:33:05 +0100

    Remove unnecessary comments from the test

--- server/microservices/authentication/src/controllers/test/currentUser.test.ts
+++ server/microservices/authentication/src/controllers/test/currentUser.test.ts
@@ -1,46 +1,27 @@
-//Mocks
-//We mock(simulate) because we don't want to sent real Http Request
-//Unit Test: Calls the handler and checks if the correct status & response are returned 
-
-//mock the http request and response (setting own mockup function ->)
-//There's probably some library for mocking the express Request and Response
-
-//aslo mock some date to send and recieve
-
 import { Request, Response } from 'express';
 import { AuthPayloadInterface, AuthUserInterface } from '@veckovn/growvia-shared';
 import { getCurrentUser, resendVerificationEmail } from '@authentication/controllers/currentUser';
-// import { getUserByID } from '@authentication/services/auth';
 import * as authService from "@authentication/services/auth";
-// import { publishMessage } from '@authentication/rabbitmqQueues/producer';
-import crypto from 'crypto';
-//getCurrentUser
-
 
 //Mock user data that getUserByID function returns
 export const mockAuthUser: AuthUserInterface = {
     id: 1,
     username: 'novak12',
-    // password?: 'tokenVPassword';
     email: "novak12@gmail.com",
     cloudinaryProfilePublicId: "12312312CLPID",
     profilePicture: "not yet uploaded to cloudinary",
     verificationEmailToken:  "299bcc43b6f8c8e720ff482744e0adcceccb7cac7a7a543e7b6101fbd688d13a",
     resetPasswordToken: null,
-    // expiresResetPassword: new Date("2025-02-30 17:31:08.381")
 }
 
-//Mock the entire module 
-jest.mock('@authentication/services/auth');
-jest.mock("@authentication/rabbitmqQueues/producer");
-jest.mock("crypto");
+//Mock the entire module  (jest.mock)
+// jest.mock('@authentication/services/auth');
+// jest.mock("@authentication/rabbitmqQueues/producer");
 
-//if we want to mock a single fucntion use spyON
+//if we want to mock a single fucntion use spyOn
 //jest.spyOn(require/import , funcName);
 //for example getUserByID auth Service function
 //jest.spyOn(require('@authentication/services/auth'), 'getUserByID')
-
-
 
 //With jest.spyOn
 describe("GetCurrentUser", () =>{
@@ -63,7 +44,6 @@ describe("GetCurrentUser", () =>{
         } as unknown as Response
 
         //jest.mock("@authentication/service/auth") without jest.spyOn
-         
     })
 
     afterEach(() => {
@@ -72,22 +52,7 @@ describe("GetCurrentUser", () =>{
       });
 
     it("should return current user", async() =>{
-        //Mock user data that getUserByID function returns
-        // const getUserByIDSpy = jest.spyOn(require('@authentication/services/auth'), 'getUserByID')
-        // .mockResolvedValue(mockAuthUser);
-
-
-        //WITH MOCK ENTIRE MODULE -> jest.mock("@authentication/service/auth")
-        // //jest.mock("@authentication/service/auth") without jest.spyOn
-        // // Directly mock getUserByID since the module is already mocked
-        // (getUserByID as jest.Mock).mockResolvedValue(mockAuthUser);
-
-
-        //IN THIS FILE WE MOCK MORE FUNCTIONS FROM AUTH SERVICE SO THE spyOn SHOULD BE REPLACED WITH mock.jest(@authentication/service/auth)
-        //with jest.spyOn mock a single functuion () import * as authService from "@authentication/services/auth";
-        //getUserByID from serivice, beacuse this 'getCurrentUser' controlelr func depends on it
-        jest.spyOn(authService, 'getUserByID').mockResolvedValue(mockAuthUser);
-    
+        jest.spyOn(authService, 'getUserByID').mockResolvedValue(mockAuthUser); 
 
         // Act: Call the function under test
         await getCurrentUser(req, res);
@@ -135,27 +100,18 @@ describe('resendVerificationEmail method', () => {
         } as unknown as Response
     })
 
-
     afterEach(() => {
         jest.restoreAllMocks();
       });
 
-    //without mocking token with 'crypto' -> caused problems
     it("should resend verification email", async () =>{
-
-        //mocking updateEmailVerification, publisMessage and getUserByID
         jest.spyOn(authService, 'updateEmailVerification').mockResolvedValue(true);//1 as true
         const publishMessageSpy = jest.spyOn(require("@authentication/rabbitmqQueues/producer"), "publishMessage").mockResolvedValue(true);//1 as true
         jest.spyOn(authService, 'getUserByID').mockResolvedValue(mockAuthUser);//1 as true
         
         await resendVerificationEmail(req, res);
 
-        // Assert (verify the behaviour)
-        //user id = 1, and randomBytes generated token ("mockToken") -> updateEmailVerification(1 , mockToken); , and token
         expect(authService.updateEmailVerification).toHaveBeenCalled();        
-        //or use spy defined variable with mocked updateEmailVerification
-        // const updateEmailVerificationSpy = jest.spyOn(authService, 'updateEmailVerification').mockResolvedValue(true);
-        /// expect(updateEmailVerificationSpy).toHaveBeenCalled();
         expect(publishMessageSpy).toHaveBeenCalled();
         expect(authService.getUserByID).toHaveBeenCalledWith(1); //userID = 1
             
