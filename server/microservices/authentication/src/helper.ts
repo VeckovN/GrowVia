cfb9826fe0855242d67a0028a822f6aa6fc6ea89
VeckovN <novaknolewow@gmail.com>
2025-02-08 19:59:44 +0100

    Add helper folder with some functions

---
+++ server/microservices/authentication/src/helper.ts
@@ -0,0 +1,34 @@
+import { AuthUserInterface } from "@veckovn/growvia-shared"
+
+const mapAuthUser = (row:any): AuthUserInterface => {
+    return {
+        id: row.id,
+        username: row.username,
+        email: row.email,
+        password: row.password,
+        userType: row?.usertype,
+        ...(row.password && { password: row.password }), //inlcude it if exists in the row
+        cloudinaryProfilePublicId: row.cloudinaryprofilepublicid, // Map to camelCase
+        profilePicture: row.profilepicture,
+        verificationEmailToken: row.verificationemailtoken,
+        resetPasswordToken: row.resetpasswordtoken,
+        expiresResetPassword: row.expiresresetpassword,
+        // createdAt: row.createdat, // If needed
+    }
+}
+
+const getExchangeNameAndRoutingKey = (userType:string): {exchangeName:string, routingKey:string} => {
+    switch(userType){
+        case 'farmer':
+            return {exchangeName: 'auth-user-farmer', routingKey: 'auth-user-farmer-key'}
+        case 'customer':
+            return {exchangeName: 'auth-user-customer', routingKey: 'auth-user-customer-key'}
+        default:
+            return {exchangeName: "", routingKey: ""}
+    }
+}
+
+export {
+    mapAuthUser,
+    getExchangeNameAndRoutingKey
+}
