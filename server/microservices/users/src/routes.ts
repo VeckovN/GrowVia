9eaf33f8079f33de3cf1da0e561b8b6025951ddc
VeckovN <novaknolewow@gmail.com>
2025-02-10 18:19:29 +0100

    feat: Add customer and farmer users rotues

--- server/microservices/users/src/routes.ts
+++ server/microservices/users/src/routes.ts
@@ -2,10 +2,11 @@ import { Application } from "express";
 import { healthRoutes } from "@users/routes/health";
 import { customerRoutes } from "@users/routes/customer";
 import { farmerRoutes } from "@users/routes/farmer";
+import { verifyGateway } from "@veckovn/growvia-shared";
 
 const BASE_PATH ='/api/v1/users' //ApiGateway -> AuthService
 export function appRoutes(app:Application): void {
     app.use('', healthRoutes()); 
-    app.use(`${BASE_PATH}/customer`, customerRoutes());  // api/v1/auth/{exported Routes from authRoutes -> /signup, /signin}
-    app.use(`${BASE_PATH}/farmer`, farmerRoutes());
+    app.use(`${BASE_PATH}/customer`, verifyGateway, customerRoutes());  // api/v1/auth/{exported Routes from authRoutes -> /signup, /signin}
+    app.use(`${BASE_PATH}/farmer`, verifyGateway, farmerRoutes());
 }
