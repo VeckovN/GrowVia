1db37ff106bdb92f3394716d72bc103aa6655dc5
VeckovN <novaknolewow@gmail.com>
2025-02-14 18:08:32 +0100

    test: add new test cases for customer services

--- server/microservices/users/src/services/test/customer.test.ts
+++ server/microservices/users/src/services/test/customer.test.ts
@@ -5,7 +5,15 @@
 // Example: Testing createCustomer to ensure it correctly creates a user in the database.
 
 import { CustomerModel } from '@users/models/customer';
-import { getCustomerByUsername, getCustomerByEmail } from '@users/services/customer';
+import { CustomerDocumentInterface } from '@veckovn/growvia-shared';
+import { 
+    getCustomerByUsername, 
+    getCustomerByEmail, 
+    updateCustomerDataByID, 
+    updateCustomerWishlist, 
+    updateCustomerSavedFarmers
+} from '@users/services/customer';
+
 
 //Just mock DB layers(CustomerModel functions)
 jest.mock('@users/models/customer', () => ({
@@ -15,9 +23,11 @@ jest.mock('@users/models/customer', () => ({
         findOne: jest.fn().mockReturnValue({
             exec: jest.fn(),
         }),
-
+        // create: jest.fn(),
         findOneAndUpdate: jest.fn(),
-        updateOne: jest.fn()
+        updateOne: jest.fn().mockReturnValue({ //because the updateOne is called with .exec
+            exec: jest.fn(),
+        }),
     }
 }))
 
@@ -59,3 +69,79 @@ describe('getCustomerByUsername and getCustomerByEmail', () => {
         expect(result).toBeNull();
     });
 });
+
+describe('updateCustomerDataByID', () => {
+    const mockCustomerID = '67ab171a53bc80962a3686fd';
+    const mockUpdateData:CustomerDocumentInterface = {
+        username:"newUsername",
+        location:{
+            country: "Serbia",
+            city: "Belgrade",
+            address: "Brace Jerkovic 11"
+        }
+    }
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should update customer data by ID', async () => {
+        (CustomerModel.findOneAndUpdate as jest.Mock).mockResolvedValue(mockUpdateData);
+        const result = await updateCustomerDataByID(mockCustomerID, mockUpdateData);
+        expect(CustomerModel.findOneAndUpdate).toHaveBeenCalledWith(
+            { _id: mockCustomerID },
+            { $set: mockUpdateData },
+            { new:true }
+        );
+        expect(result).toEqual(mockUpdateData);
+    })
+
+    it("should return null if customer isn't found ", async() => {
+        (CustomerModel.findOneAndUpdate as jest.Mock).mockResolvedValue(null);
+        const result = await updateCustomerDataByID('noexistID', mockUpdateData); 
+        expect(result).toBeNull();
+    })
+})
+
+describe('updateCustomerWishlist and updateCustomerWishlist', () => {
+    const mockCustomerID ='customer123';
+    const mockProductID ='product123';
+    const mockFarmerID ='farmer123';
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should add a product to a wishlish', async () =>{
+        //not returning any value, just calling the method
+        await updateCustomerWishlist(mockCustomerID, mockProductID, 'add');
+        expect(CustomerModel.updateOne).toHaveBeenCalledWith(
+            { _id: mockCustomerID },
+            { $addToSet: { wishlist: mockProductID } }
+        )
+    })
+
+    it('should remove a product from the wishlist', async() => {
+        await updateCustomerWishlist(mockCustomerID, mockProductID, 'remove');
+        expect(CustomerModel.updateOne).toHaveBeenCalledWith(
+            { _id: mockCustomerID },
+            { $pull: { wishlist: mockProductID } }
+        )
+    })
+
+    it('should add a farmer to the saved list', async () => {
+        await updateCustomerSavedFarmers(mockCustomerID, mockFarmerID, 'add');
+        expect(CustomerModel.updateOne).toHaveBeenCalledWith(
+            { _id: mockCustomerID },
+            { $addToSet: { savedFarmes: mockFarmerID } }
+        )
+    })
+
+    it('should remove a farmer from the saved list', async () => {
+        await updateCustomerSavedFarmers(mockCustomerID, mockFarmerID, 'remove');
+        expect(CustomerModel.updateOne).toHaveBeenCalledWith(
+            { _id: mockCustomerID },
+            { $pull: { savedFarmes: mockFarmerID } }
+        )
+    })
+})
