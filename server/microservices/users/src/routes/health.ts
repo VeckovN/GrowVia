672c7fc8cb1458213dc75bce3f445f6bc6b46cde
VeckovN <novaknolewow@gmail.com>
2025-02-17 18:07:54 +0100

    feat: add health route for checking status

--- server/microservices/users/src/routes/health.ts
+++ server/microservices/users/src/routes/health.ts
@@ -1,8 +1,9 @@
 import express, { Router} from "express";
+import { health } from "@users/controllers/health";
 
 const router:Router = express.Router();
 
 export const healthRoutes = (): Router => {
-    // router.get('/gateway-health', health); 
+    router.get('/users-health', health); 
     return router;
 };
