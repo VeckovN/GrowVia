42ef738948f6fbae78d54f080b8587ff7da192ab
VeckovN <novaknolewow@gmail.com>
2025-02-20 11:08:31 +0100

    fix: change wrong req body props

--- server/microservices/product/src/model/product.ts
+++ server/microservices/product/src/model/product.ts
@@ -1,7 +1,7 @@
-// import mongoose, { Model } from 'mongoose';
-// import { ProductDocumentInterface } from '@veckovn/growvia-shared';
-// import { ProductSchema } from "@product/schema/product";
+import mongoose, { Model } from 'mongoose';
+import { ProductDocumentInterface } from '@veckovn/growvia-shared';
+import { ProductSchema } from "@product/schema/product";
 
-// const ProductModel: Model<ProductDocumentInterface> =  mongoose.model<ProductDocumentInterface>("Product", ProductSchema, "Product");
-// export { ProductModel }
+const ProductModel: Model<ProductDocumentInterface> =  mongoose.model<ProductDocumentInterface>("Product", ProductSchema, "Product");
+export { ProductModel }
 
