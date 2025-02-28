a4938a78610f3f975c6456a3fcd95dfda3024e54
VeckovN <novaknolewow@gmail.com>
2025-02-28 10:24:01 +0100

    Fix: sintax error, correct 'search' prefix

--- server/microservices/product/src/routes/product.ts
+++ server/microservices/product/src/routes/product.ts
@@ -15,9 +15,7 @@ const router:Router = express.Router();
 export const productRoutes = (): Router => {
     router.post('/create', productCreate); 
     router.get('/:productID', productByID); 
-    // /serach/:from/:size/:type?query=''?minPrice=''?maxPrice=''
-    //query,maxPrice and minPrice is part of url (directly added)
-    router.get('/serach/:from/:size/:type', searchProducts);  
+    router.get('/search/:from/:size/:type', searchProducts);  
     router.get('/search/similar/:productID', getMoreProductsLikeThis); 
     router.get('/category/:category', productsByCategory); 
     router.get('/farmer/:productID', farmersProductsByID); 
