aa5c148cfcad700ddff94f9a0043e6e891ccfee0
VeckovN <novaknolewow@gmail.com>
2025-03-13 19:13:29 +0100

    fix: typescript type error removed with installing other @types/amqplib version

--- server/microservices/order/src/rabbitmqQueues/rabbitmq.ts
+++ server/microservices/order/src/rabbitmqQueues/rabbitmq.ts
@@ -1,20 +1,20 @@
 
-// import { winstonLogger } from "@veckovn/growvia-shared";
-// import { Logger } from "winston";
-// import client, { Channel, Connection } from 'amqplib';
-// import { config } from '@order/config';
+import { winstonLogger } from "@veckovn/growvia-shared";
+import { Logger } from "winston";
+import client, { Channel, Connection } from 'amqplib';
+import { config } from '@order/config';
 
-// const log:Logger = winstonLogger(`${config.ELASTICSEARCH_URL}`, 'orderRabbitMQConnection', 'debug');
+const log:Logger = winstonLogger(`${config.ELASTICSEARCH_URL}`, 'orderRabbitMQConnection', 'debug');
 
-// export async function createConnection():Promise<Channel | undefined> {
-//     try{
-//         const connection:Connection = await client.connect(`${config.RABBITMQ_AMQP_ENDPOINT}`);
-//         const channel: Channel = await connection.createChannel();
-//         log.info('Product service connected to RabbitMQ successfully');
-//         return channel;
-//     }
-//     catch(error){
-//         log.log('error', 'Product service RabbitMQ connection failed: ', error);
-//         return undefined;
-//     }
-// }
+export async function createConnection():Promise<Channel | undefined> {
+    try{
+        const connection:Connection = await client.connect(`${config.RABBITMQ_AMQP_ENDPOINT}`);
+        const channel: Channel = await connection.createChannel();
+        log.info('Product service connected to RabbitMQ successfully');
+        return channel;
+    }
+    catch(error){
+        log.log('error', 'Product service RabbitMQ connection failed: ', error);
+        return undefined;
+    }
+}
