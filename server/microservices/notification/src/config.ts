2cb32d33a67888eb049870bf8baa62bb64fd9ded
VeckovN <novaknolewow@gmail.com>
2025-03-23 21:42:48 +0100

    add: databaseUrl, ClientUrl, GatewayJwtToken and JWT_Token props

--- server/microservices/notification/src/config.ts
+++ server/microservices/notification/src/config.ts
@@ -3,24 +3,32 @@ dotenv.config();
 
 interface Config {
     NODE_ENV: string | undefined;
+    API_GATEWAY_URL: string | undefined;
     CLIENT_URL: string | undefined;
     ELASTICSEARCH_URL: string | undefined;
     ELASTICSEARCH_APM_SERVER_URL: string | undefined;
     ELASTICSEARCH_APM_TOKEN: string | undefined;
+    DATABASE_URL: string | undefined;
     RABBITMQ_AMQP_ENDPOINT: string | undefined;
     TEST_EMAIL_NAME: string | undefined;
     TEST_EMAIL: string | undefined;
     TEST_EMAIL_PASSWORD: string | undefined;
+    GATEWAY_JWT_TOKEN: string | undefined;
+    JWT_TOKEN: string | undefined;
 }
 
 export const config: Config = {
     NODE_ENV: process.env.NODE_ENV || '',
+    API_GATEWAY_URL: process.env.API_GATEWAY_URL || '',
     CLIENT_URL: process.env.CLIENT_URL || '',
     ELASTICSEARCH_URL: process.env.ELASTICSEARCH_URL || '', 
     ELASTICSEARCH_APM_SERVER_URL: process.env.ELASTICSEARCH_APM_SERVER_URL || '',
     ELASTICSEARCH_APM_TOKEN: process.env.ELASTICSEARCH_APM_TOKEN || '',
+    DATABASE_URL: process.env.DATABASE_URL || '',
     RABBITMQ_AMQP_ENDPOINT: process.env.RABBITMQ_AMQP_ENDPOINT || '',
     TEST_EMAIL_NAME: process.env.TEST_EMAIL_NAME || '',
     TEST_EMAIL: process.env.TEST_EMAIL || '',
     TEST_EMAIL_PASSWORD: process.env.TEST_EMAIL_PASSWORD || '',
+    GATEWAY_JWT_TOKEN: process.env.GATEWAY_JWT_TOKEN || '123123',
+    JWT_TOKEN: process.env.JWT_TOKEN || '123123'
 };
