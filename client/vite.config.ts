effa64a237bcfdbdf6e9335d823603a0695f5e69
VeckovN <novaknolewow@gmail.com>
2025-05-12 22:17:48 +0200

    feat: add client folder with initial Vite React setup and configuration

---
+++ client/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tscongPaths from 'vite-tsconfig-paths';
+
+// https://vite.dev/config/
+export default defineConfig({
+  root: '.',
+  plugins: [
+    react({
+      //Our compontents are with .tsx, 
+      //and every time we have changes, we want to have automatical reload
+      include: '**/*.tsx'
+    }),
+    //and every time we have changes, we want to have automatical reload
+    tscongPaths() 
+  ],
+  resolve: {
+    alias: {
+      src: '/src'
+    }
+  },
+  build: {
+    outDir: './build'
+  },
+  //defaul is :5173 but we can change it to our "CLIENT_URL" which is :3000
+  server: {
+    port: 3000
+  }
+});
