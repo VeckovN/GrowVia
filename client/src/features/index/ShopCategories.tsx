6c88abc44e1663331572b21b6713464ddc91d6bf
VeckovN <novaknolewow@gmail.com>
2025-05-27 22:29:26 +0200

    change: use useVisibleCount custom hook to get repsective count item depends on width screen

--- client/src/features/index/ShopCategories.tsx
+++ client/src/features/index/ShopCategories.tsx
@@ -1,10 +1,17 @@
 import {FC, ReactElement } from 'react';
 import { productCategories } from '../shared/utils/data';
 // import { v4 as uuidv4 } from 'uuid';
+import useVisibleCount from '../hooks/useVisibleCount';
 
 import CategoryItem from '../index/CategoryItem';
 
 const ShopCategories: FC = (): ReactElement => {
+    const visibleCount = useVisibleCount({
+        mobile:2,
+        tablet:4,
+        desktop:7
+    }) 
+
     return (
         <section className='bg-white pt-10'>
             <h2 className='pb-5 font-bold text-xl text-center sm:text-2xl'>
@@ -18,7 +25,7 @@ const ShopCategories: FC = (): ReactElement => {
                 </div>
 
                 <div className='flex overflow-hidden max-w-full  space-x-2'>
-                {productCategories.map((category) => (
+                {productCategories.slice(0, visibleCount).map((category) => (
                     <CategoryItem 
                         id = {category.id}
                         name = {category.name}
