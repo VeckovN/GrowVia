46274746102556ed73ecfa7a27f8f3617962d493
VeckovN <novaknolewow@gmail.com>
2025-05-28 23:02:09 +0200

    change: move files to new folder and add hover elements on product item

---
+++ client/src/features/shared/productsList/ProductSlideItem.tsx
@@ -0,0 +1,87 @@
+import {FC, ReactElement } from 'react';
+import { ProductItemInterface } from '../utils/data';
+
+import { VscHeart } from "react-icons/vsc";
+//<VscHeart />
+import { VscHeartFilled } from "react-icons/vsc";
+{/* <VscHeartFilled /> */}
+
+const ProductSlideItem: FC<ProductItemInterface> = ({
+    id, 
+    name, 
+    category,
+    unit,
+    farmerName,
+    farmerLocation,
+    price,
+    favorite,
+    image
+}): ReactElement => {
+    return (
+        <div key={id} className='
+            group flex flex-col my-4 justify-centers items-center
+            border-2 border-greyB rounded-lg sm:w-full sm:max-w-[320px] lg:mx-1 bg-white 
+            hover:opacity-90 hover:shadow-lg transition-shadow duration-100'
+        >
+            <div className='relative w-full h-[170px]  sm:h- bg-cover bg-center'
+                style={{backgroundImage: `url(${image})`}}>
+                
+                <div className='
+                    hidden w-24 h-14 absolute top-[-2px] left-[-2px] bg-white opacity-80
+                    text-lg font-semibold font-lato rounded flex justify-center items-center
+                    group-hover:flex
+
+                '
+                >
+                    {favorite 
+                    ? <VscHeartFilled className='text-3xl text-red-500'/>
+                    : <VscHeart className='text-3xl text-red-500'/>
+                    }
+                </div>
+
+                <div className='
+                    hidden w-24 h-14 absolute top-[-2px] right-[-2px] bg-white opacity-80
+                    text-lg font-semibold font-lato rounded flex justify-center items-center
+                    group-hover:flex
+                '
+                >
+                    ${price}
+                </div>
+            </div>
+
+            <div className='w-full px-3 py-2 grid grid-cols-3 items-center'>
+                <div className='text-greyC text-sm truncate text-left pr-2'>
+                    {category}
+                </div>
+                
+                <div className='text-center mx-auto'>
+                    <h3 className='text-lg font-medium truncate max-w-[200px] px-1'>
+                        {name}
+                    </h3>
+                </div>
+                
+                <div className='text-greyC text-sm truncate text-right pl-2'>
+                    {unit}
+                </div>
+            </div>
+
+
+            <div className='flex w-full px-2 pt-2 pb-2 items-center justify-between'>
+                <div className='flex flex-col max-w-[70%] '>
+                    {/* use 'truncate' to prevent overflowing text -> automatically adding '...'on end of text */}
+                    <h4 className='text-sm truncate'>
+                        {farmerName}
+                    </h4>
+                    <p className='font-lato text-xs text-greyC' >
+                        {farmerLocation}
+                    </p>
+                </div>
+                <button className='px-3 py-1 flex-shrink-0 text-sm border-2 rounded border-greyB hover:bg-grey'>
+                    + Add
+                </button>
+            </div>
+        </div>  
+    )
+}
+
+export default ProductSlideItem;
---
+++ client/src/features/shared/productsList/ProductsSlideList.tsx
@@ -0,0 +1,80 @@
+import { FC, ReactElement } from 'react';
+import useVisibleCount from '../../hooks/useVisibleCount';
+import { SlideListInterface, productsList  } from '../utils/data';
+import ProductSlideItem from './ProductSlideItem';
+import CircleArrowIconButton from '../../shared/CircleArrowIconButton';
+import { GoChevronLeft } from "react-icons/go";
+import { GoChevronRight } from "react-icons/go";
+import { GoChevronUp } from "react-icons/go";
+import { GoChevronDown } from "react-icons/go";
+
+
+const ProductsSlideList: FC<SlideListInterface> = ({title}): ReactElement => {
+    const visibleCount = useVisibleCount({
+        mobile:3,
+        tablet:2,
+        desktop:5
+    }) 
+
+    return (
+        <section className='container mx-auto px-7 pt-10 max-w-[400px] sm:max-w-[700px] lg:max-w-[1320px] '>
+
+            <div className='flex justify-between px-2 pb-3 items-center '>
+                <div>
+                    <h2 className='font-bold text-2xl mb-2 '> {title} </h2>
+                     <div className='hidden sm:flex sm:justify-items-start '>
+                        <CircleArrowIconButton onClick={()=>alert("Left")}>
+                            <GoChevronLeft className='text-2xl pl-[-20px]'/>
+                        </CircleArrowIconButton>
+                        <CircleArrowIconButton onClick={()=>alert("Right")}>
+                            <GoChevronRight className='text-2xl pl-[-20px]'/>
+                        </CircleArrowIconButton>
+                    </div>
+                </div>
+                <button className='font-lato text-sm sm:text-base border rounded-lg border-black p-2 hover:bg-grey'>All Products {'>'}</button>
+            </div>
+
+            <div className='flex justify-center mb-2 sm:hidden'> 
+                <CircleArrowIconButton onClick={()=>alert("Up")}>
+                    <GoChevronUp className='text-3xl'/>
+                </CircleArrowIconButton>
+            </div>
+
+            {/* 
+                on mobile view display 3 items in flex-col, 
+                on Table view diplay 2 items  in flex-row
+                on Larger view display 4 items in flex-row as well
+            */}
+            {/*max-w-[1200px] mx-auto = prevents content from stretching to wide and center it  */}
+            <div className='
+                grid grid-cols-1 gap-4         /* Mobile: 1 column */
+                sm:grid-cols-2 sm:gap-6        /* Tablet: 2 columns */
+                lg:grid-cols-4 lg:gap-2         /* Desktop: 4 columns */
+            '>
+                {/* {productsList.slice(0,4).map((product) => ( */}
+                {productsList.slice(0, visibleCount).map((product) => (
+                    <ProductSlideItem 
+                        id={product.id}
+                        name={product.name}
+                        category={product.category}
+                        unit={product.unit}
+                        farmerName={product.farmerName}
+                        farmerLocation={product.farmerLocation}
+                        price={product.price}
+                        favorite={product.favorite}
+                        image={product.image}
+                    />
+                ))}
+            </div>
+
+            <div className='flex justify-center mb-2 sm:hidden'> 
+                <CircleArrowIconButton onClick={()=>alert("Down")}>
+                    <GoChevronDown className='text-3xl'/>
+                </CircleArrowIconButton>
+            </div>
+          
+        </section>
+    )
+}
+
+export default ProductsSlideList;
