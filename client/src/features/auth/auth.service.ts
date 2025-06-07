cf05ae5b3a9271452629ee821f7076e5e628706b
VeckovN <novaknolewow@gmail.com>
2025-06-07 22:00:05 +0200

    fix: add correct Response interface

--- client/src/features/auth/auth.service.ts
+++ client/src/features/auth/auth.service.ts
@@ -1,12 +1,15 @@
 import { api } from "../../store/api";
 import { AuthUserInterface, SignUpPayloadInterface, SignInPayloadInterface} from "./auth.interfaces";
+import { ResponseInterface } from '../shared/interfaces';
 
 //api -> already created (use same instance)
 
 const authApi = api.injectEndpoints({ //in api(createApi) we'we created endpoints as empty callback func
     //we inject endpoint for individual 'service/feature'
     endpoints: (build) => ({
-        signUp: build.mutation<AuthUserInterface, SignUpPayloadInterface>({
+        //we got response as { message,user }  that implements ResponseInterface 
+        //the user fetch data is in 'user' proop
+        signUp: build.mutation<ResponseInterface, SignUpPayloadInterface>({
             query(body: SignUpPayloadInterface) {
                 return {
                     url: '/auth/signup',
@@ -18,7 +21,7 @@ const authApi = api.injectEndpoints({ //in api(createApi) we'we created endpoint
             //Want to invalidate it in the cache (new data fetched)  -> with "Auth" Tag
             invalidatesTags: ['Auth']
         }),
-        signIn: build.mutation<AuthUserInterface, SignInPayloadInterface>({
+        signIn: build.mutation<ResponseInterface, SignInPayloadInterface>({
             query(body: SignInPayloadInterface) {
                 return {
                     url: '/auth/signin',
@@ -28,7 +31,7 @@ const authApi = api.injectEndpoints({ //in api(createApi) we'we created endpoint
             },
             invalidatesTags: ['Auth']
         }),
-        logout: build.mutation<AuthUserInterface, void>({
+        logout: build.mutation<ResponseInterface, void>({
             query() {
                 return {
                     url: '/auth/signout',
