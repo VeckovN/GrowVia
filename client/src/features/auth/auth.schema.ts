bb231261c1a25764a7a3857dca04cefd7d29f9b6
VeckovN <novaknolewow@gmail.com>
2025-06-10 22:12:11 +0200

    refactor: remove object passing to require option

--- client/src/features/auth/auth.schema.ts
+++ client/src/features/auth/auth.schema.ts
@@ -1,31 +1,47 @@
-import { ObjectSchema, object, string, array, mixed  } from 'yup';
+import { ObjectSchema, object, string, array, mixed, ref } from 'yup';
 
-import { SignUpPayloadInterface, SignInPayloadInterface } from './auth.interfaces';
+import { SignUpFormInterface, SignInPayloadInterface } from './auth.interfaces';
 import { LocationInterface, FarmerLocationInterface } from './auth.interfaces';
 
-const signupSchema: ObjectSchema<SignUpPayloadInterface> = object({
-    
+const signUpSchema: ObjectSchema<SignUpFormInterface> = object({
+
     username: string()
         .required('Username is required')
         .min(3, 'Username must be at least 3 characters'),
-    
+
     email: string()
         .required('Email is required')
         .email('Invalid email format'),
 
     password: string()
         .required('Password is required')
-        .min(6, 'Password must be at least 6 characters'),
+        .min(5, 'Password must be at least 5 characters'),
+
+    repeatPassword: string()
+        .required('Please confirm your password')
+        .oneOf([ref('password')], 'Passwords must match'),
 
     userType: mixed<'customer' | 'farmer'>()
         .oneOf(['customer', 'farmer'], 'Invalid user type')
         .required('User type is required'),
 
     profilePicture: string().required('Profile picture is required'),
+    backgroundImage: string().optional(),
 
-    fullName: string()
-        .required('Full name is required')
-        .min(6, 'Full name must be at least 6 characters'),
+    firstName: string()
+        .required('First name is required')
+        .min(3, 'First name must be at least 3 characters'),
+
+    lastName: string()
+        .required('Last First name is required')
+        .min(3, 'Full name must be at least 3 characters'),
+
+    phoneNumber: string()
+        .required('Phone number is required')
+        .matches(
+            /^(?:\+3816\d{7,8}|06\d{7,8})$/,
+            'Enter a valid Serbian phone number'
+        ),
 
     location: object().required('Location is required') as ObjectSchema<LocationInterface | FarmerLocationInterface>,
     farmName: string().optional(),
@@ -33,9 +49,9 @@ const signupSchema: ObjectSchema<SignUpPayloadInterface> = object({
     socialLlinks: array(string().required().url('Each social link must be a valid URL')).optional()
 })
 
-const signinSchema: ObjectSchema<SignInPayloadInterface> = object({
-    usernameOrEmail: string().required({ username: 'Username is required'}).min(3, { username: 'Username is a required field'}),
-    password: string().required({ password: 'Password is required' }).min(4, { password: 'Password is arequired field' })
+const signInSchema: ObjectSchema<SignInPayloadInterface> = object({
+    usernameOrEmail: string().required('Username is required' ).min(3, 'Username must be at least 3 characters'),
+    password: string().required('Password is required' ).min(5, 'Password must be at least 5 characters')
 })
 
-export { signupSchema, signinSchema }
+export { signUpSchema, signInSchema }
