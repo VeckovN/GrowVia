225c667182533bfd04e6ea2c6d705b26814e1304
VeckovN <novaknolewow@gmail.com>
2025-06-10 22:11:18 +0200

    Add resetValidationError function as returned hook property

--- client/src/features/auth/auth.interfaces.ts
+++ client/src/features/auth/auth.interfaces.ts
@@ -1,3 +1,5 @@
+import { ObjectSchema } from "yup";
+import { ValidationErrorMap } from "./hooks/useAuthValidation";
 
 // import {} //from user feature
 export interface LocationInterface {
@@ -11,15 +13,17 @@ export interface FarmerLocationInterface extends LocationInterface {
     longitude: string
 }
 
-export interface AuthUserResponseInterface {
+export interface AuthUserInterface {
+
+    //with RTK Query reqeust we got props in camelCase (on testing with RESTClient .http we got lowcase props)
     id: string,
     username: string,
     email: string,
-    usertype: string,
-    verificationemailtoken: string | null,
-    resetpasswordtoken: string | null,
-    expireresetpassword: Date | null,
-    createdat: Date,
+    userType: string,
+    verificationEmailToken: string | null,
+    resetPasswordToken: string | null,
+    expireResetPassword: Date | null,
+    createdAt: string, //redux toolkit use serializable data -> this have to be cast to string 
     fullName: string,
     location: LocationInterface | FarmerLocationInterface,
     profilePicture: string,
@@ -34,14 +38,42 @@ export interface AuthUserResponseInterface {
     socialLinks?: string[],
 }
 
+
+//on RTQ query HTTP request we should get a lot of different props
+export interface ResponseInterface{
+    message?: string,
+    user?: AuthUserInterface
+}
+
 export interface SignUpPayloadInterface {
     username: string;
     email: string;
     password: string;
+    repeatPassword: string,
+    phoneNumber: string,
     userType: 'customer' | 'farmer';
     profilePicture: string;
-    fullName: string;
-    location: LocationInterface | FarmerLocationInterface;
+    backgroundImage?: string; //for farmers
+    fullName: string; 
+    location?: LocationInterface | FarmerLocationInterface;
+    farmName?: string;
+    description?: string;
+    socialLlinks?: string[];
+}
+
+//fullname is actually firstName + LastName 
+export interface SignUpFormInterface {
+    username: string;
+    email: string;
+    password: string;
+    repeatPassword: string,
+    phoneNumber: string,
+    userType: 'customer' | 'farmer';
+    profilePicture: string;
+    backgroundImage?: string; //for farmers
+    firstName: string;
+    lastName: string;
+    location?: LocationInterface | FarmerLocationInterface;
     farmName?: string;
     description?: string;
     socialLlinks?: string[];
@@ -50,4 +82,29 @@ export interface SignUpPayloadInterface {
 export interface SignInPayloadInterface {
     usernameOrEmail: string;
     password: string;
+}
+
+export interface ResetPasswordPayloadInterface {
+    email: string;
+}
+
+export interface useAuthValidationInterface{
+    //allow only auth related schemas
+    schema: ObjectSchema<SignInPayloadInterface | SignUpPayloadInterface | ResetPasswordPayloadInterface>; //specific schema
+    userData: SignInPayloadInterface | SignUpPayloadInterface | ResetPasswordPayloadInterface;
+}
+
+export interface RoleOptionInterface {
+    title: string,
+    description: string,
+    selected: boolean,
+    icon: string;
+    onSelect: () => void;
+}
+
+export interface CustomerFormOptionPropsInterface {
+    userInfo: SignUpFormInterface;
+    setUserInfo: React.Dispatch<React.SetStateAction<SignUpFormInterface>>;
+    validationErrors: ValidationErrorMap; //-> Record<string,string> -> the useAuthValidation validation hook return value
+    actionError: string;
 }
