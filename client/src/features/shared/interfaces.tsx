235e685afc039af51e1f0f4c1c82e753270cf40c
VeckovN <novaknolewow@gmail.com>
2025-06-12 20:30:21 +0200

    add ResponseInterface

--- client/src/features/shared/interfaces.tsx
+++ client/src/features/shared/interfaces.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { ChangeEvent, KeyboardEvent } from 'react';
+import { AuthUserInterface } from '../auth/auth.interfaces';
 
 export interface CircleIconButtonInterface {
     onClick: () => void;
@@ -12,15 +13,6 @@ export interface MarketplaceInfoInterface {
     icon: string
 }
 
-export interface CustomerAuthInterface {
-    
-}
-
-export interface HeaderInterface {
-    userType: string,
-    user: CustomerAuthInterface | null ;
-}
-
 export interface HeaderIconBadgeInterface {
     icon: string,
     alt: string,
@@ -29,4 +21,45 @@ export interface HeaderIconBadgeInterface {
     textClassName?:string, //additional classes for text div
     className?: string, //additional parrent div classes
     onClick: () => void;
+}
+
+export interface TextFieldPropsInterface {
+    className?: string;
+    id?: string;
+    name?: string;
+    type?: string;
+    value?: string | number;
+    placeholder?: string;
+    readOnly?: boolean;
+    checked?: boolean;
+    autoFocus?: boolean;
+    maxLength?: number;
+    min?: string | number;
+    max?: string | number;
+    // onChange?: (event: React.ChangeEvent) => void; => ChangeEvent is too generic
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void; 
+    onClick?: () => void;
+    onFocus?: () => void;
+    onBlur?: () => void;
+    onKeyUp?: () => void;
+    // onKeyDown?: (event: KeyboardEvent) => void;
+    onKeyDown?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+}
+
+export interface SelectFieldPropsInterface {
+    id: string;
+    name: string;
+    value: string;
+    onChange?: (e: ChangeEvent<HTMLSelectElement>) => void;
+    options: Array<{ value: string; label: string }>;
+    className?: string;
+    placeholder?: string;
+}
+
+//Generic interface
+//on RTQ query HTTP request we should get a lot of different props *based on service
+export interface ResponseInterface{
+    token?: string;
+    message?: string;
+    user?: AuthUserInterface; //we got response for authUser like {message, user} => on ApiGateway as res.json({message,user})
 }
