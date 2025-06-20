020e8602d74feb93313541b997016a4d1e6a6d9c
VeckovN <novaknolewow@gmail.com>
2025-06-20 14:32:13 +0200

    feat: create Modal component

---
+++ client/src/features/shared/Modal.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react"
+
+const Modal = ({children, closeModal}: {children: ReactNode, closeModal:()=>void}) => {
+
+    return (
+        <div className='fixed inset-0 z-50 overflow-y-auto '>
+            <div 
+                className="fixed inset-0 bg-black bg-opacity-70 backdrop-blur-sm"
+                onClick={closeModal}
+            />
+            {/* Modal Container */}
+            <div className='flex min-h-full items-center justify-center p-4 text-center '>
+                <div className='max-w-[700px] relative transform overflow-hidden rounded-lg bg-whit text-left shadow-xl transition-all'>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal
