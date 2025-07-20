52e924d98b63267913b5e76aed83c4b03959e80f
VeckovN <novaknolewow@gmail.com>
2025-07-20 15:40:11 +0200

    style: remove text-white from spinner component

--- client/src/features/shared/page/LoadingSpinner.tsx
+++ client/src/features/shared/page/LoadingSpinner.tsx
@@ -1,11 +1,11 @@
 import { FC, ReactElement } from "react";
 import { FaSpinner } from "react-icons/fa";
 
-const LoadingSpinner:FC = ():ReactElement => {
+const LoadingSpinner:FC<{spinnerClassName?: string}> = ({spinnerClassName}):ReactElement => {
 
     return(
         <div className='mx-auto w-full flex text-center'>
-            <FaSpinner className='text-white text-4xl mx-auto bg-none animate-spin'/>
+            <FaSpinner className={`text-4xl mx-auto bg-none animate-spin ${spinnerClassName}`}/>
         </div>
     )
 }
