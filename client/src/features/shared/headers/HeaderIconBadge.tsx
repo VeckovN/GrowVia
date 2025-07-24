c87e8f7b922f422956b814367e63a345ae34ea42
VeckovN <novaknolewow@gmail.com>
2025-07-24 19:37:53 +0200

    fix: prevent rendering badge content when content is undefined or less than 1

--- client/src/features/shared/headers/HeaderIconBadge.tsx
+++ client/src/features/shared/headers/HeaderIconBadge.tsx
@@ -12,16 +12,19 @@ const HeaderIconBadge: FC<HeaderIconBadgeInterface> = ({
     onClick
 }): ReactElement => {
     return (
-        <div className={`flex items-center mr-1 cursor-pointer group ${className}`} onClick={onClick}>
+        <div 
+            className={`flex items-center mr-1 cursor-pointer group ${className}`} 
+            onClick={onClick}
+        >
             <div className='relative group-hover:scale-105'>
                 <img 
-                    className='h-8 w-8'
+                    className='h-8 w-8 rounded-full'
                     src={icon}
                     alt={alt}
                 />
 
-                {content &&
-                <div className='
+                {content !== undefined  && content > 0 && 
+                 <div className='
                     absolute flex justify-center items-center rounded-full w-5 h-5 bg-green7 top-[-.5em] right-0
                     text-white text-xs font-semibold
                 '>
@@ -32,7 +35,6 @@ const HeaderIconBadge: FC<HeaderIconBadgeInterface> = ({
 
             {text && 
             <div className={`${hiddenText ? 'hidden' : 'flex'} md:flex md:ml-1 group-hover:text-gray-700 ${textClassName}`}>
-                {/* user.username */}
                 {text}
             </div>
             }
@@ -40,12 +42,4 @@ const HeaderIconBadge: FC<HeaderIconBadgeInterface> = ({
     )
 }
 
-
 export default HeaderIconBadge;
-
-
-
-{/* <div className='
-    py-2 px-2 w-[260px] md:w-auto md:min-w-[300px] md:max-w-[500px] hidden sm:flex justify-center items-center bg-grey
-    rounded-xl border-2 border-greyB
-'></div> */}
