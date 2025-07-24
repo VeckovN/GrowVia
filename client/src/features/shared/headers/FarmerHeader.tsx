625a02e5cbfcf5f9639787edfba6622256b11c98
VeckovN <novaknolewow@gmail.com>
2025-07-24 19:32:20 +0200

    feat: display authUser profileAvatar instead of CustomerTestIcon

--- client/src/features/shared/headers/FarmerHeader.tsx
+++ client/src/features/shared/headers/FarmerHeader.tsx
@@ -10,6 +10,8 @@ import LogoIcon from '../../../assets/header/LogoIcon.svg';
 import Bell from '../../../assets/header/Bell.svg';
 import CustomerTestIcon from '../../../assets/header/CustomerTest.svg';
 
+//Commit: add avatar image from authUser state
+
 const FarmerHeader:FC = () => {
     const authUser = useAppSelector((state: ReduxStateInterface) => state.authUser)
     const profileDropdownRef = useRef<HTMLDivElement>(null);
@@ -24,7 +26,7 @@ const FarmerHeader:FC = () => {
     }
 
     return (
-        <header className='w-full bg-red-100 flex justify-between items-center h-14 px-2 py-9'>
+        <header className='w-full flex justify-between items-center h-14 px-2 py-9'>
             <div className='flex items-center relative'>
                 <img
                     className='w-8 h-8'
@@ -45,12 +47,12 @@ const FarmerHeader:FC = () => {
                 <HeaderIconBadge
                     icon={Bell}
                     alt="notifications"
-                    content='5'
+                    content={5}
                     onClick={() => alert("User Notifications")}
                 />
 
                 <HeaderIconBadge
-                    icon={CustomerTestIcon}
+                    icon={authUser?.profileAvatar?.url ?? CustomerTestIcon}
                     alt="avatar"
                     text={authUser.username}
                     textClassName='md:flex'
@@ -59,7 +61,7 @@ const FarmerHeader:FC = () => {
                 />
 
                 {isProfileOpen &&
-                    <div ref={profileDropdownRef} className='absolute top-10'>
+                    <div ref={profileDropdownRef} className='absolute top-10 z-10'>
                         <ProfileDropdown authUser={authUser} closeProfileDropdown={closeProfileDropdown} />
                     </div>
                  }
