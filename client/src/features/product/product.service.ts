4346a1106bec18a26b648355645d4af47ed789b1
VeckovN <novaknolewow@gmail.com>
2025-07-26 16:38:08 +0200

    refactor: add from, size and sort pagination options to getPRoductsByFarmerID query

--- client/src/features/product/product.service.ts
+++ client/src/features/product/product.service.ts
@@ -1,8 +1,7 @@
 import { ResponseInterface } from "../shared/interfaces";
 import { api } from "../../store/api";
+import { CreateProductInterface, ProductSearchParamsInterface } from "./product.interface";
 
-// import { CreateProductInterface, ProductDocumentInterface} from "./product.interface";
-import { CreateProductInterface } from "./product.interface";
 
 export const productApi = api.injectEndpoints({
     endpoints: (build) => ({
@@ -27,8 +26,12 @@ export const productApi = api.injectEndpoints({
             invalidatesTags: ['Product']
         
         }),
-        getProductByFarmerID: build.query<ResponseInterface, string>({
-            query: (farmerID: string) => `product/farmer/${farmerID}`,
+
+        getProductByFarmerID: build.query<ResponseInterface, { farmerID: string; from?: number; size?: number; sort?: string }>({
+            query: ({ farmerID, from = 0, size = 12, sort = 'newest' }) => ({
+                url: `product/farmer/${farmerID}`,
+                params: { from, size, sort } //this data trough req.query -> this params is part of req.query on express
+            }),
             providesTags: ['Product']
         }),
 
@@ -42,14 +45,32 @@ export const productApi = api.injectEndpoints({
             providesTags: ['Product']
         }),
 
+        getProductsSearch: build.query<ResponseInterface, ProductSearchParamsInterface>({
+            query: (paramsData) => (
+                {
+                url: 'product/search',
+                params: {
+                    ...paramsData,
+                    // Convert array to comma-separated string for URL
+                    subCategories: paramsData.subCategories?.join(','),
+                    from: paramsData.from?.toString(),
+                    size: paramsData.size?.toString(),
+                    minPrice: paramsData.minPrice?.toString(),
+                    maxPrice: paramsData.maxPrice?.toString(),
+                    quantity: paramsData.quantity?.toString()
+                }
+            }),
+            providesTags: ['Product']
+        }),
+
     })
 })
-
 
 export const {
     useCreateProductMutation,
     useDeleteProductMutation,
     useGetProductByFarmerIDQuery,
     useGetNewestProductsQuery,
-    useGetProductByIDQuery
+    useGetProductByIDQuery,
+    useGetProductsSearchQuery
 } = productApi
